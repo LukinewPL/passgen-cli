1415a952556b9d35bd1a9c66d8fb7f142b6ad1e7
LukinewPL <kuzniarlukasz2010@gmail.com>
2026-04-02 21:54:41 +0200

    feat: added password generation function using secrets

--- passgen.py
+++ passgen.py
@@ -2,6 +2,7 @@ import click
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument('generate', help='Generate a password')
 parser.add_argument('--length', default=16, help='Password length')
 parser.add_argument('--count', default=1, help='Password count')
 parser.add_argument('--no-symbols', default=False, help='If password has symbols')
@@ -18,4 +19,20 @@ def cli():
 @click.option('--no-symbols', default=False, help='If password has symbols')
 @click.option('--no-digits', default=False, help='If password has digits')
 def generate(length, count, no_symbols, no_digits):
-    pass
+    import secrets
+    import string
+    
+    alphabet = string.ascii_letters
+    if not no_digits:
+        alphabet += string.digits
+    if not no_symbols:
+        alphabet += string.punctuation
+    
+    for _ in range(count):
+        password = ''.join(secrets.choice(alphabet) for _ in range(length))
+        click.echo(password)
+
+cli.add_command(generate)
+
+if __name__ == '__main__':
+    cli()
